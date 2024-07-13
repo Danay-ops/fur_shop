ee5705823ea9308da507017be981e85186b2b259
danya <danya@uni-dubna.ru>
2024-07-14 00:46:11 +0300

    Added fixtures for goods

--- main/views.py
+++ main/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
 # Create your views here.
+from goods.models import Categories
 def index(request):
+    categories = Categories.objects.all()
 
-    return render(request, 'main/index.html')
+    context = {
+        'categories': categories
+    }
+
+    return render(request, 'main/index.html', context)
